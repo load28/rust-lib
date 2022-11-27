feb0516cd6fc3e6949cbc9e3d8e17e7007925a80
load28 <tjalsdud89@naver.com>
2022-11-27 14:10:36 +0900

    add test

--- src/tests/tests.rs
+++ src/tests/tests.rs
@@ -1,14 +1,26 @@
-use super::super::collection;
+use super::super::collection::{vec};
 
 #[cfg(test)]
-pub mod tests {
-    use super::collection;
+pub mod collection_test {
+    use super::vec::{new_vec};
 
     #[test]
-    fn vec() {
-        collection::vec::new_vec();
+    fn vec_fn() {
+        new_vec();
 
         let array = vec![1, 2, 3];
+        let value = match array.get(4) {
+            Some(value) => value,
+            None => &30,
+        };
+    }
+}
+
+#[cfg(test)]
+pub mod array_test {
+    #[test]
+    fn array() {
+        let array = [1, 2, 3];
         let value = match array.get(4) {
             Some(value) => value,
             None => &30,
