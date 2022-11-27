bc475d73e3e6d57ee4913180203e12ec2788724b
load28 <tjalsdud89@naver.com>
2022-11-27 20:05:51 +0900

    add test

--- src/tests/tests.rs
+++ src/tests/tests.rs
@@ -21,9 +21,62 @@ pub mod array_test {
     #[test]
     fn array() {
         let array = [1, 2, 3];
-        let value = match array.get(4) {
+        let _value = match array.get(4) {
             Some(value) => value,
             None => &30,
         };
+    }
+
+    #[test]
+    fn enum_vec() {
+        #[derive(Debug)]
+        enum SpreadsheetCell {
+            Int(i32),
+            Float(f64),
+            Text(String),
+        }
+
+        let row = vec![
+            SpreadsheetCell::Int(3),
+            SpreadsheetCell::Text(String::from("blue")),
+            SpreadsheetCell::Float(10.12),
+        ];
+
+        enum User {
+            Admin(String),
+            User(i32),
+        }
+
+        let users = vec![
+            User::Admin(String::from("admin")),
+            User::User(1),
+        ];
+
+        let _none = &String::from("NONE");
+        let user_value = match users.get(1) {
+            Some(value) => match value {
+                User::Admin(x) => x,
+                _ => _none,
+            },
+            None => _none,
+        };
+
+        if user_value != _none {
+            println!("user_value: {}", user_value);
+        }
+
+        let row = row.get(1);
+        let value = match row {
+            Some(value) => {
+                if let SpreadsheetCell::Int(value2) = value {
+                    value2
+                } else {
+                    &0
+                }
+            },
+            None => &0,
+        };
+
+        println!("row: {:?}", value);
     }
 }
