65adcef7fbdcdb67a83d9a22afef02c416678da4
load28 <tjalsdud89@naver.com>
2022-11-27 14:04:28 +0900

    move test files

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
 pub mod network;
 pub mod client;
 pub mod collection;
-
-#[cfg(test)]
 mod tests;
--- src/tests/tests.rs
+++ src/tests/tests.rs
@@ -1,4 +1,4 @@
-use super::collection;
+use super::super::collection;
 
 #[cfg(test)]
 pub mod tests {
@@ -11,7 +11,7 @@ pub mod tests {
         let array = vec![1, 2, 3];
         let value = match array.get(4) {
             Some(value) => value,
-            None() => 30,
+            None => &30,
         };
     }
 }
